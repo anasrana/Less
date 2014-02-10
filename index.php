c91756ebfb03bc70baf4320720282c933052f620
anasrana <anas.rana@gmail.com>
2014-02-10 10:48:25 +0000

    no real change

--- index.php
+++ index.php
@@ -23,26 +23,26 @@
 
 <header id="masthead" class="site-header" role="banner">
 	<div class="container">
-		
+
 		<div class="gravatar">
-			<?php 
+			<?php
 				// grab admin email and their photo
 				$admin_email = get_option('admin_email');
-				echo get_avatar( $admin_email, 100 ); 
+				echo get_avatar( $admin_email, 100 );
 			?>
 		</div><!--/ author -->
-		
+
 		<div id="brand">
 			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a> &mdash; <span><?php echo get_bloginfo( 'description' ); ?></span></h1>
 		</div><!-- /brand -->
-	
+
 		<nav role="navigation" class="site-navigation main-navigation">
 			<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
 		</nav><!-- .site-navigation .main-navigation -->
-		
+
 		<div class="clear"></div>
 	</div><!--/container -->
-		
+
 </header><!-- #masthead .site-header -->
 
 <div class="container">
@@ -55,16 +55,16 @@
 	/*-----------------------------------------------------------------------------------*/
 	/* Start Home loop
 	/*-----------------------------------------------------------------------------------*/
-	
+
 	if( is_home() || is_archive() ) {
-	
+
 ?>
 			<?php if ( have_posts() ) : ?>
 
 				<?php while ( have_posts() ) : the_post(); ?>
 
 					<article class="post">
-					
+
 						<h1 class="title">
 							<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
 								<?php the_title() ?>
@@ -76,24 +76,24 @@
 									<?php comments_popup_link( __( 'Comment', 'break' ), __( '1 Comment', 'break' ), __( '% Comments', 'break' ) ); ?>
 								</span>
 							<?php endif; ?>
-						
+
 						</div><!--/post-meta -->
-						
+
 						<div class="the-content">
 							<?php the_content( 'Continue...' ); ?>
-							
+
 							<?php wp_link_pages(); ?>
 						</div><!-- the-content -->
-						
+
 						<div class="meta clearfix">
 							<div class="category"><?php echo get_the_category_list(); ?></div>
 							<div class="tags"><?php echo get_the_tag_list( '| &nbsp;', '&nbsp;' ); ?></div>
 						</div><!-- Meta -->
-						
+
 					</article>
 
 				<?php endwhile; ?>
-				
+
 				<!-- pagintation -->
 				<div id="pagination" class="clearfix">
 					<div class="past-page"><?php previous_posts_link( 'Newer &raquo;' ); ?></div>
@@ -102,21 +102,21 @@
 
 
 			<?php else : ?>
-				
+
 				<article class="post error">
 					<h1 class="404">Nothing posted yet</h1>
 				</article>
 
 			<?php endif; ?>
 
-		
+
 	<?php } //end is_home(); ?>
 
 <?php
 	/*-----------------------------------------------------------------------------------*/
 	/* Start Single loop
 	/*-----------------------------------------------------------------------------------*/
-	
+
 	if( is_single() ) {
 ?>
 
@@ -126,7 +126,7 @@
 				<?php while ( have_posts() ) : the_post(); ?>
 
 					<article class="post">
-					
+
 						<h1 class="title"><?php the_title() ?></h1>
 						<div class="post-meta">
 							<?php if( comments_open() ) : ?>
@@ -134,24 +134,24 @@
 									<?php comments_popup_link( __( 'Comment', 'less' ), __( '1 Comment', 'less' ), __( '% Comments', 'less' ) ); ?>
 								</span>
 							<?php endif; ?>
-						
+
 						</div><!--/post-meta -->
-						
+
 						<div class="the-content">
 							<?php the_content( 'Continue...' ); ?>
-							
+
 							<?php wp_link_pages(); ?>
 						</div><!-- the-content -->
-						
+
 						<div class="meta clearfix">
 							<div class="category"><?php echo get_the_category_list(); ?></div>
 							<div class="tags"><?php echo get_the_tag_list( '| &nbsp;', '&nbsp;' ); ?></div>
-						</div><!-- Meta -->						
-						
+						</div><!-- Meta -->
+
 					</article>
 
 				<?php endwhile; ?>
-				
+
 				<?php
 					// If comments are open or we have at least one comment, load up the comment template
 					if ( comments_open() || '0' != get_comments_number() )
@@ -160,7 +160,7 @@
 
 
 			<?php else : ?>
-				
+
 				<article class="post error">
 					<h1 class="404">Nothing posted yet</h1>
 				</article>
@@ -169,12 +169,12 @@
 
 
 	<?php } //end is_single(); ?>
-	
+
 <?php
 	/*-----------------------------------------------------------------------------------*/
 	/* Start Page loop
 	/*-----------------------------------------------------------------------------------*/
-	
+
 	if( is_page()) {
 ?>
 
@@ -183,21 +183,21 @@
 				<?php while ( have_posts() ) : the_post(); ?>
 
 					<article class="post">
-					
+
 						<h1 class="title"><?php the_title() ?></h1>
-						
+
 						<div class="the-content">
 							<?php the_content(); ?>
-							
+
 							<?php wp_link_pages(); ?>
 						</div><!-- the-content -->
-						
+
 					</article>
 
 				<?php endwhile; ?>
 
 			<?php else : ?>
-				
+
 				<article class="post error">
 					<h1 class="404">Nothing posted yet</h1>
 				</article>
@@ -220,9 +220,11 @@
 <footer class="site-footer" role="contentinfo">
 	<div class="site-info container">
 		<?php do_action( 'break_credits' ); ?>
-		<a href="http://wordpress.org/" title="A Semantic Personal Publishing Platform" rel="generator">Proudly powered by WordPress</a>
-		<span class="sep"> and </span>
-		<a href="http://lessmade.com/themes/less" rel="theme">LESS</a> by <a href="http://jarederickson.com" rel="designer">Jared Erickson</a>
+		<a href="mailto:anas.rana@gmail.com">anas.rana@gmail.com</a>
+		<span class="sep"> • </span>
+		<a href="http://go.warwick.ac.uk/anasrana">Official Website</a>
+		<br>
+		<a >Copyright Anas Ahmad Rana, 2014</a>
 	</div><!-- .site-info -->
 </footer><!-- #colophon .site-footer -->
 
